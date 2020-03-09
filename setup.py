10bd261efba003f791b9244ea5bf9cfe86d6bbeb
elf <arya@foursquare.com>
2020-03-09 12:02:28 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
     author="Edmund Dipple",
     author_email="elmundio1987@gmail.com",
     description="A library that provides asserts for testing Terraform configuration",
-    url="https://github.com/elmundio87/terraform_validate",
-    download_url = 'https://github.com/elmundio87/terraform_validate/tarball/2.8.0',
+    url="https://github.com/foursquare/terraform_validate",
+    download_url = 'https://github.com/foursquare/terraform_validate/tarball/2.8.0',
     keywords = ['terraform', 'assert', 'testing'],
     packages = find_packages(),
     install_requires=[
