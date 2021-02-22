e06d6f38f76188977fa6936e4ff174c6e39e5acf
Daniel Nomura <dnomura@dnomura.local>
2021-02-22 10:27:54 -0800

    update pyhcl version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="terraform_validate",
-    version="3.0.0",
+    version="3.0.1",
     author="Edmund Dipple",
     author_email="elmundio1987@gmail.com",
     description="A library that provides asserts for testing Terraform configuration",
     url="https://github.com/foursquare/terraform_validate",
-    download_url = 'https://github.com/foursquare/terraform_validate/tarball/3.0.0',
+    download_url = 'https://github.com/foursquare/terraform_validate/tarball/3.0.1',
     keywords = ['terraform', 'assert', 'testing'],
     packages = find_packages(),
     install_requires=[
